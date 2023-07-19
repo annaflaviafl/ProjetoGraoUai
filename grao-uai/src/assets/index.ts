71e3b931b0688267d48af2d26d4a21aa9327a7d0
annaflaviafl <annaflavialf14@gmal.com>
2023-07-19 11:48:26 -0300

    #8: Atualiza pastas e arquivos necessários

---
+++ grao-uai/src/assets/index.ts
@@ -0,0 +1,7 @@
+import imageBackground from '../assets/imageBackground.png';
+import imageButtonContent from '../assets/imageButtonContent.png';
+import imageContacts from '../assets/imageContacts.png';
+import imageLogo from '../assets/imageLogo.png';
+import imageMainContent from '../assets/imageMainContent.png';
+
+export { imageBackground, imageButtonContent, imageContacts, imageLogo, imageMainContent };
---
+++ grao-uai/src/assets/produtos/index.ts
@@ -0,0 +1,7 @@
+import imageBuy from '../produtos/imageBuy.png';
+import imageDoceAroma from '../produtos/imageDoceAroma.png';
+import imageExpresso from '../produtos/imageExpresso.png';
+import imagePopularTitle from '../produtos/imagePopularTitle.png';
+import imageSerraDourada from '../produtos/imageSerraDourada.png';
+
+export { imageBuy, imageDoceAroma, imageExpresso, imagePopularTitle, imageSerraDourada };
